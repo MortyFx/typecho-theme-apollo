9788d3a1375a1f4e2bf3522477a19b3e21e28e80
MortyFx <17744239+MortyFx@users.noreply.github.com>
2017-07-28 10:03:54 +0800

    add archive page

--- archive.php
+++ archive.php
@@ -1,36 +1,38 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-    <div class="col-mb-12 col-8" id="main" role="main">
-        <h3 class="archive-title"><?php $this->archiveTitle(array(
+   
+<h3 class="archive-title"><?php $this->archiveTitle(array(
             'category'  =>  _t('分类 %s 下的文章'),
             'search'    =>  _t('包含关键字 %s 的文章'),
             'tag'       =>  _t('标签 %s 下的文章'),
             'author'    =>  _t('%s 发布的文章')
-        ), '', ''); ?></h3>
-        <?php if ($this->have()): ?>
-    	<?php while($this->next()): ?>
-            <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
-    			<h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-    			<ul class="post-meta">
-    				<li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li>
-    				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></li>
-    				<li><?php _e('分类: '); ?><?php $this->category(','); ?></li>
-                    <li itemprop="interactionCount"><a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
-    			</ul>
-                <div class="post-content" itemprop="articleBody">
-        			<?php $this->content('- 阅读剩余部分 -'); ?>
-                </div>
-    		</article>
-    	<?php endwhile; ?>
-        <?php else: ?>
-            <article class="post">
+), '', ''); ?></h3>
+
+<ul class="home post-list">
+    <?php if ($this->have()): ?>
+	<?php while($this->next()): ?>
+
+	<li class="post-list-item">
+                    <article class="post-block">
+                        <h2 class="post-title"><a href="<?php $this->permalink() ?>" class="post-title-link"><?php $this->title() ?></a></h2>
+                        <div class="post-info"><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('F j, Y'); ?></time></div>
+                        <div class="post-content">
+                            <?php print_summary($this, 255);?>
+                        </div><a href="<?php $this->permalink() ?>" class="read-more">Read more »</a></article>
+				</li>
+				
+    <?php endwhile; ?>
+     <?php else: ?>
                 <h2 class="post-title"><?php _e('没有找到内容'); ?></h2>
-            </article>
-        <?php endif; ?>
+    <?php endif; ?>
 
-        <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
-    </div><!-- end #main -->
 
-	<?php $this->need('sidebar.php'); ?>
+<nav class="paginator"> 
+    <div class="paginator-doc">PAGE
+<?php if($this->_currentPage>1) echo $this->_currentPage;  else echo 1;?> OF <?php echo   ceil($this->getTotal() / $this->parameter->pageSize); ?></div>
+    <?php $this->pageLink('PREV PAGE'); ?>
+<?php $this->pageLink('NEXT PAGE','next'); ?></nav>
+
+
 	<?php $this->need('footer.php'); ?>
