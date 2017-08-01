e8055657e5286ebd06c81d08523efca95dbb2a4c
MortyFx <17744239+MortyFx@users.noreply.github.com>
2017-08-01 18:08:39 +0800

    v1.0.0

--- footer.php
+++ footer.php
@@ -12,16 +12,20 @@
 </div>
 <?php $this->footer(); ?>
 <script src="<?php $this->options->themeUrl('js/main.js'); ?>"></script>
-<script src="//cdn.staticfile.org/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
-<script src="<?php $this->options->themeUrl('js/prism.js'); ?>" data-no-instant></script>
-<script data-no-instant>
-    InstantClick.on('change', function (isInitialLoad) {
-        if (isInitialLoad === false) {
-            if (typeof Prism !== 'undefined') Prism.highlightAll(true, null);
-        }  
-    });
-    InstantClick.init();
-</script>
+<?php if (!empty($this->options->pjax) && in_array('InstantClick', $this->options->pjax)): ?>
+    <script src="//cdn.staticfile.org/instantclick/3.0.1/instantclick.min.js" data-no-instant></script>
+    <script src="<?php $this->options->themeUrl('js/prism.js'); ?>" data-no-instant></script>
+    <script data-no-instant>
+        InstantClick.on('change', function (isInitialLoad) {
+            if (isInitialLoad === false) {
+                if (typeof Prism !== 'undefined') Prism.highlightAll(true, null);
+            }
+        });
+        InstantClick.init();
+    </script>
+<?php else: ?>
+    <script src="<?php $this->options->themeUrl('js/prism.js'); ?>" data-no-instant></script>
+<?php endif; ?>
 </body>
 
 </html>
